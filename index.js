ea07dea8c110803e38cb64525036285f567d1ed3
Suraj <moonstoper@dev.github.com>
2020-05-11 00:12:15 +0530

    just saving

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const passport=require("passport");
-const GoogleStrategy=require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 passport.use(new GoogleStrategy());
 
-
-
-
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
