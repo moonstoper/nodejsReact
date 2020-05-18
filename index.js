7737a60d3e3fec59573c9cf76958c5dbbd5658cb
Suraj <moonstoper@dev.github.com>
2020-05-18 21:22:11 +0530

    setup authentication and cookie

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const mongoose=require("mongoose");
+const cookie=require("cookie-session");
+const passport=require("passport");
 const keys=require("./config/keys");
 require("./models/Users");
 require("./services/passport");
 mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(
+    cookie({
+        maxAge:30*24**60*60*1000,
+        keys:[keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,18 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
+//making cookie from id
+passport.serializeUser((user,done)=>{
+  done(null,user.id);
+});
+//with cookie creating user model instance
+passport.deserializeUser((id,done)=>{
+  User.findById(id).then(user=>{
+    done(null,user);
+  })
+}
+);
+
 passport.use(
   new GoogleStrategy(
     {
@@ -11,9 +23,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
-      console.log("accessToken:",profile.id);
-      
+      User.findOne({ googleID: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //already documented
+          done(null,existingUser);
+        } else {
+          new User({ googleID: profile.id }).save().then(user=>done(null,user));
+        }
+      });
     }
   )
 );
