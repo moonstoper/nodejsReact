3ee34ea16e2b3aef0ffe7be4515b89e79e33f6ca
Suraj <moonstoper@dev.github.com>
2020-05-26 19:25:26 +0530

    reinserting

--- client/src/components/Landing.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Landing=()=>{
-    return (<div className="uk-text-center">Landing</div>);
-
-
-}
-
-export default Landing;
--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducers";
-export default combineReducers({
-  auth: authReducer,
-});
